bba4383787734f8347f7e38ca06f341feb563f92
josethz00 <jose.soares18@fatec.sp.gov.br>
2024-01-08 21:14:56 -0300

    feat: hello world

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gofiber/fiber/v3"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	app.Listen(":8778")
+}
