5088c24b5dabfc7fc63e5a33153a5a9f2d2b05d5
josethz00 <jose.soares18@fatec.sp.gov.br>
2024-01-10 10:35:31 -0300

    feat: check secret access key

--- utils/keys.go
+++ utils/keys.go
@@ -3,3 +3,7 @@ package utils
 func CheckAccessKey(accessKey string) bool {
 	return accessKey == "123456"
 }
+
+func CheckSecretAccessKey(secretAccessKey string) bool {
+	return secretAccessKey == "123456"
+}
