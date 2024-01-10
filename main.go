f0912608ab7995a4ea0ad97d48623ffc4d82c466
josethz00 <jose.soares18@fatec.sp.gov.br>
2024-01-10 10:45:56 -0300

    apply middleware

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"github.com/gofiber/fiber/v3"
+	"github.com/josethz00/build-your-own-s3/utils"
+)
 
 func main() {
 	app := fiber.New()
+
+	app.Use(func(c fiber.Ctx) error {
+		accessKey := c.Get("x-access-key")
+		secretAccessKey := c.Get("x-secret-access-key")
+
+		if accessKey == "" || secretAccessKey == "" {
+			return c.Status(401).JSON(fiber.Map{
+				"message": "INVALID ACCESS KEY OR SECRET ACCESS KEY",
+			})
+		}
+
+		if utils.CheckApiCredentials(accessKey, secretAccessKey) {
+			return c.Next()
+		}
+
+		return c.Status(401).JSON(fiber.Map{
+			"message": "INVALID ACCESS KEY OR SECRET ACCESS KEY",
+		})
+	})
 
 	app.Get("/", func(c fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
