2b670d9dd3195661ca6a8908cc34ae6d3406776a
josethz00 <jose.soares18@fatec.sp.gov.br>
2024-03-02 11:53:15 -0300

    feat(sql): create base tables

--- initdb.sql
+++ initdb.sql
@@ -1,24 +1,21 @@
-CREATE TABLE IF NOT EXISTS `buckets` (
-  `id` bigint unsigned NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `public` boolean NOT NULL DEFAULT false,
-  `description` text,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
+CREATE TABLE IF NOT EXISTS buckets (
+  id bigint NOT NULL,
+  name varchar(255) NOT NULL,
+  public boolean NOT NULL DEFAULT false,
+  description text,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS `files` (
-  `id` bigint unsigned NOT NULL,
-  `bucket_id` bigint unsigned NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `size` bigint unsigned NOT NULL,
-  `mime_type` varchar(255) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`),
-  KEY `bucket_id` (`bucket_id`),
-  CONSTRAINT `files_ibfk_1` FOREIGN KEY (`bucket_id`) REFERENCES `buckets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE IF NOT EXISTS files (
+  id bigint NOT NULL,
+  bucket_id bigint NOT NULL,
+  name varchar(255) NOT NULL UNIQUE,
+  size bigint NOT NULL,
+  mime_type varchar(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT files_ibfk_1 FOREIGN KEY (bucket_id) REFERENCES buckets (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
